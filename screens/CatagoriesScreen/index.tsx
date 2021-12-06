70ea05826f2f35e624c830043710c6c15fa02b0d
aliraza944 <alisabri022@gmail.com>
2021-12-06 22:06:08 +0500

    setup an installation

---
+++ screens/CatagoriesScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const CatagoriesScreen = () => {
+  return (
+    <View>
+      <Text>Catagories screen</Text>
+    </View>
+  );
+};
+
+export default CatagoriesScreen;
