8390a6ddb0c288d40b940efb359cda692209b5c7
aliraza944 <alisabri022@gmail.com>
2021-12-07 17:21:19 +0500

    improved the styling for the meals card

--- screens/CatagoriesScreen/index.tsx
+++ screens/CatagoriesScreen/index.tsx
@@ -6,24 +6,42 @@ import {
   Button,
   FlatList,
   TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
 } from "react-native";
 import { CATEGORIES, colors } from "../../assets/Data/data";
 const CatagoriesScreen = (props: any) => {
+  let TouchableComponent: any = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 22) {
+    TouchableComponent = TouchableNativeFeedback;
+  }
+
   const renderItem = (itemData: any): any => {
     return (
-      <TouchableOpacity
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: "CategoryMeals",
-            params: {
-              categoryId: itemData.item.id,
-            },
-          });
-        }}
-        style={styles.gridItem}
-      >
-        <Text>{itemData.item.title}</Text>
-      </TouchableOpacity>
+      <View style={styles.gridItem}>
+        <TouchableComponent
+          onPress={() => {
+            props.navigation.navigate({
+              routeName: "CategoryMeals",
+              params: {
+                categoryId: itemData.item.id,
+              },
+            });
+          }}
+          style={{ flex: 1 }}
+        >
+          <View
+            style={{
+              ...styles.container__gridItem,
+              ...{ backgroundColor: itemData.item.color },
+            }}
+          >
+            <Text style={styles.title} numberOfLines={2}>
+              {itemData.item.title}
+            </Text>
+          </View>
+        </TouchableComponent>
+      </View>
     );
   };
 
@@ -39,6 +57,25 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  container__gridItem: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 15,
+    textAlign: "right",
   },
 });
 
