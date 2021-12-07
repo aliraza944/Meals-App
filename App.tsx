4c5712b94904ead8e241b85c4957f52cb2fc95d2
aliraza944 <alisabri022@gmail.com>
2021-12-07 10:27:07 +0500

    App loading screen added to display until the foonts are loaded

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppLoading from "expo-app-loading";
+import * as Fonts from "expo-font";
+const fetchFont = () => {
+  return Fonts.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={() => setFontLoaded(true)}
+        onError={() => console.log("something went wrong")}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
