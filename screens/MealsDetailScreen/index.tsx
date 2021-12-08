ff84ee0c6971fe928bc1397c998199ca8166aa81
aliraza944 <alisabri022@gmail.com>
2021-12-08 11:37:45 +0500

    Added the dynamic header for MealDetail screen

--- screens/MealsDetailScreen/index.tsx
+++ screens/MealsDetailScreen/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
-
+import { MEALS } from "../../assets/Data/data";
 const MealsDetailScreen = (props: any) => {
   const mealId = props.navigation.getParam("mealId");
-
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>MealsDetails</Text>
+      <Text>{selectedMeal?.title}</Text>
       <Button
         title="Go to Categories "
         onPress={() => props.navigation.popToTop()}
@@ -14,6 +14,15 @@ const MealsDetailScreen = (props: any) => {
     </View>
   );
 };
+
+MealsDetailScreen.navigationOptions = (navigationData: any): any => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal?.title,
+  };
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
